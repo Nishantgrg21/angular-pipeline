8093d441c0905837a8968d3d77b2af59408bb221
Nishant Garg <75825264+Nishantgrg21@users.noreply.github.com>
2025-08-12 16:29:15 +0530

    Update app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,7 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-@Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
-})
+@Componen
 export class App {
   protected readonly title = signal('angular-App');
 }
